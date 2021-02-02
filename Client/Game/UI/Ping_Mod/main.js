08a520eaef3a149451884ea5a9ad46a2854ada8b
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-02 12:11:31 +0100

    Delete main.js

--- Client/Game/UI/Ping_Mod/main.js
+++
@@ -1,7 +0,0 @@
-this.object.Ping_Display.style.padding = "30px";
-this.object.Ping_Display.style.font = "30px Nunito,sans-serif";
-this.object.Ping_Display.style.display = "none";
-this.object.Ping_Display.style.position = "fixed";
-this.object.Ping_Display.style.top = "0px";
-this.object.Ping_Display.style.left = "130px";
-this.object.Ping_Display.style.color = "#FFFFFF0";
