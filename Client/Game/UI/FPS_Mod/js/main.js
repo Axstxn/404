d276e759a9e16021d02c48f9b133d6be62082a0c
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-02 11:59:50 +0100

    Create main.js
    Main.js for FPS

---
+++ Client/Game/UI/FPS_Mod/js/main.js
@@ -0,0 +1,8 @@
+this.object.FPS_Display.style.padding = "30px";
+this.object.FPS_Display.style.font = "30px Nunito,sans-serif";
+this.object.FPS_Display.style.display = "none";
+this.object.FPS_Display.style.position = "fixed";
+this.object.FPS_Display.style.top = "-35px";
+this.object.FPS_Display.style.left = "130px";
+this.object.FPS_Display.style.color = "#FFFFFF0";
+this.object.FPS_Display.textContent = this.fps;
