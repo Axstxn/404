9d6178d14a2bebeb94050704a469e46af16067f8
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-02 11:22:40 +0100

    Create main.js

---
+++ Client/PlayButton/JavaScript/main.js
@@ -0,0 +1,7 @@
+this.object.style.padding = "10px";
+this.object.style.font = "16px Nunito,sans-serif";
+this.object.style.display = "block";
+this.object.style.position = "fixed";
+this.object.style.top = "250px"
+this.object.style.left = "550px"
+this.object.style.color = "#fff";
