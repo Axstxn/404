f1ab9dd60af1b15c3416168f17e4aeb987ec43cc
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-02 11:26:44 +0100

    Create main.js

---
+++ Client/MainMenu/JavaScript/main.js
@@ -0,0 +1,8 @@
+document.getElementById("menuCardHolder").remove();
+document.getElementById("partyButton").remove();
+document.getElementById("linksContainer2").remove();
+document.getElementById("joinPartyButton").remove();
+document.getElementById("youtuberOf").remove();
+document.getElementById("onetrust-consent-sdk").remove();
+document.getElementById("moomooio_728x90_home").remove();
+document.getElementById("gameName").remove();
