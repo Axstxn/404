83f73247f94d89b613265df9ac5ae0b2092b529a
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-02 13:45:39 +0100

    Create main.js

---
+++ Client/SocialButtons/JavaScript/main.js
@@ -0,0 +1,6 @@
+this.object.SocialStuff.style.font = "16px Nunito,sans-serif";
+this.object.SocialStuff.style.display = "block";
+this.object.SocialStuff.style.position = "fixed";
+this.object.SocialStuff.style.top = "35px";
+this.object.SocialStuff.style.left = "0px";
+this.object.SocialStuff.style.color = "#fff";
