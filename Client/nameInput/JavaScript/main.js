936c96e9fdeb3ad896680bcd3fc8597a92d66228
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-02 22:16:12 +0100

    Create main.js

---
+++ Client/nameInput/JavaScript/main.js
@@ -0,0 +1,7 @@
+this.object.TextInput.style.padding = "10px";
+this.object.TextInput.style.font = "16px Nunito,sans-serif";
+this.object.TextInput.style.display = "block";
+this.object.TextInput.style.position = "fixed";
+this.object.TextInput.style.top = "250px";
+this.object.TextInput.style.left = "40px";
+this.object.TextInput.style.color = "#fff";
