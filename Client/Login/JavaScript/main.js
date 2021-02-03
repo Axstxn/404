ce5242ca2251f1f6e494c7d249915b7dc27141dd
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-03 17:01:36 +0100

    Create main.js

---
+++ Client/Login/JavaScript/main.js
@@ -0,0 +1,7 @@
+this.object.loginField.style.padding = "10px";
+this.object.loginField.style.font = "16px Nunito,sans-serif";
+this.object.loginField.style.display = "block";
+this.object.loginField.style.position = "fixed";
+this.object.loginField.style.top = "50px";
+this.object.loginField.style.right = "0px";
+this.object.loginField.style.color = "#fff";
