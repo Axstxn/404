2cf5aea7ee75f029d8714bcd1524e5b2790b7718
Nuro-dev <66263456+Nuro-dev@users.noreply.github.com>
2021-02-03 17:05:56 +0100

    Create main.js

---
+++ Client/Register/JavaScript/main.js
@@ -0,0 +1,7 @@
+this.object.RegisterField.style.padding = "10px";
+this.object.RegisterField.style.font = "16px Nunito,sans-serif";
+this.object.RegisterField.style.display = "block";
+this.object.RegisterField.style.position = "fixed";
+this.object.RegisterField.style.top = "50px";
+this.object.RegisterField.style.right = "0px";
+this.object.RegisterField.style.color = "#fff";
